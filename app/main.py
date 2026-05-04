52856467235a99db6cb250f85ea6aa32d6d08b1f
Sebastian Castañeda <castanedacs@gmail.com>
2026-05-04 16:34:54 +0200

    Add X-Process-Time middleware for request timing

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.api.v1.router import api_router
+from app.core.middleware import add_process_time_header
 
 
 API_V1 = "/api/v1"
@@ -14,5 +15,5 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="FastAPI Sandbox", lifespan=lifespan)
-
+app.middleware("http")(add_process_time_header)
 app.include_router(api_router, prefix=API_V1)
