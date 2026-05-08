ed413e956165fede7ec2b4238db417e1cbd37041
Sebastian Castañeda <castanedacs@gmail.com>
2026-05-08 20:55:38 +0200

    Minor fix

--- app/core/config.py
+++ app/core/config.py
@@ -4,13 +4,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str = "FastAPI Sandbox"
+    app_name: str = "FastAPI ecommerce app"
     debug: bool = False
     version: str = "0.1"
     environment: Literal["development", "staging", "production"] = "development"
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
-    database_url: str = (
-        "mysql+aiomysql://fa_ecom_user:fa_ecom_pass@localhost:3306/fa_ecom"
-    )
+    database_url: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
